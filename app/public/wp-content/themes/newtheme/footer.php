ba7cce054c7ee3de2d7302dc07f6376deb90c18e
unknown <marcin.adam.mroczko@gmail.com>
2019-04-27 15:05:01 +0200

    live search

--- app/public/wp-content/themes/newtheme/footer.php
+++ app/public/wp-content/themes/newtheme/footer.php
@@ -67,7 +67,11 @@
   			<i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
   			<input type="text" name="" class="search-term" placeholder="What are you looking for?" id="search-terms">
   			<i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
-
+  		</div>
+  	</div>
+  	<div class="container">
+  		<div id="search-overlay__results">
+  			
   		</div>
   	</div>
   </div>
--- app/public/wp-content/themes/newtheme/js/modules/Search.js
+++ app/public/wp-content/themes/newtheme/js/modules/Search.js
@@ -1,27 +1,101 @@
 import $ from 'jquery';
-
+ 
 class Search {
-	//1. Describe our and create object
+	//1. Describe and create object
 	constructor() {
+		this.resultsDiv = $('#search-overlay__results');
 		this.openButton = $('.js-search-trigger');
 		this.closeButton = $('.search-overlay__close');
 		this.searchOverlay = $('.search-overlay');
+		this.isOverlayOpen = false;
+		this.searchField = $('#search-terms');
 		this.events();
+		this.isSpinnerVisible = false;
+		this.previousValue;
+		this.typingTimer;
 	}
 	//2. Events
 	events() {
 		this.openButton.on('click', this.openOverlay.bind(this));
 		this.closeButton.on('click', this.closeOverlay.bind(this));
+		$(document).on('keydown', this.keyPressDispatcher.bind(this));
+		this.searchField.on('keyup', this.typingLogic.bind(this));
 	}
 
 	//3. Methods (functions, actions)
 	openOverlay() {
 		this.searchOverlay.addClass('search-overlay--active');
-		console.log('click');
+		$('body').addClass('body-no-scroll');
+		this.isOverlayOpen = true;
+
+		// console.log('click');
+		// console.log('its open');
 	}
 
 	closeOverlay() {
 		this.searchOverlay.removeClass('search-overlay--active');
+		$('body').removeClass('body-no-scroll');
+		this.isOverlayOpen = false;
+		// this.searchField.val('').blur();
+		// this.resultsDiv.html('');
+
+		// console.log('its close');
+	}
+
+	typingLogic() {
+
+		console.log(`previous value : ${this.previousValue}`);
+		console.log(`searchfield value: ${this.searchField.val()}`);
+
+		if (this.searchField.val() != this.previousValue) {
+
+			clearTimeout(this.typingTimer);
+
+			if (this.searchField.val()) {
+
+				if(!this.isSpinnerVisible) { 
+
+				this.resultsDiv.html('<div class="spinner-loader"></div>');
+				this.isSpinnerVisible = true;
+
+			}
+
+			this.typingTimer = setTimeout(this.getResults.bind(this), 2000);
+
+			} else {
+
+				this.resultsDiv.html('');
+				this.isSpinnerVisible = false;
+
+			}
+
+		}
+
+		this.previousValue = this.searchField.val();
+	}
+
+	getResults() {
+		this.resultsDiv.html('<p>imagine real search results here!</p>');
+		this.isSpinnerVisible = false;
+	}
+
+	keyPressDispatcher(e) {
+
+		// console.log(e.keyCode);
+
+		if(e.keyCode == 83 && !this.isOverlayOpen && !$('input, textarea').is(':focus')) {
+
+			// console.log(e.keyCode);
+			
+			this.openOverlay();
+
+		} else if(e.keyCode == 27 && this.isOverlayOpen) {
+
+			// console.log(e.keyCode);
+
+			this.closeOverlay();
+
+		}
 	}
 }
 
